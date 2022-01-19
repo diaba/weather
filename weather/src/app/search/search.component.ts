4b28c1cb6e3da3e85cc7fb13d231c90184ab233c
diaba <hwar@cincinnatistate.edu>
2022-01-19 16:06:12 -0500

    Add service to our app

--- weather/src/app/search/search.component.ts
+++ weather/src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { debounceTime, Subject } from 'rxjs'; // observable
+import {SearchService } from './search.service'; // import the service
 
 @Component({
   selector: 'app-search',
@@ -10,18 +12,24 @@ export class SearchComponent implements OnInit {
   zip: string = '';
   weather: any;
   unit:string = '';
-  constructor(private http: HttpClient) { }
+  searchSubject = new Subject();
+
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
-  }
-findWeather(zip: string, unit:string): void {
-  this.http
-      .get(
-        `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=${unit}`
-      )
+      // listen to the event
+      this.searchSubject
+      .pipe(debounceTime(1000))// pipe transform a format debounceTime delay 
+      .subscribe(zip => {
+        console.log(zip);
+      this.searchService.createAPIObservable(zip)
       .subscribe((response) => {
         console.log(response);
         this.weather = response;
       });
+      })
+  }
+findWeather(zip: string, unit:string): void {
+  this.searchSubject.next(zip); // track zip change
 }
 }
